171d6ab4e3e00567d21b023b4cb1ae2a3ac60c7d
JayThePHQ <67293191+JayThePHQ@users.noreply.github.com>
2022-01-10 10:31:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-/*eslint-env es6*/
 const options = {
   bottom: '64px', // default: '32px'
   right: '32px', // default: '32px'
   left: 'unset', // default: 'unset'
   time: '1s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
+  backgroundColor: 'white',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
